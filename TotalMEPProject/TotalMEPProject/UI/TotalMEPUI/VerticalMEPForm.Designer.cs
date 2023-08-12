9dad40f4e5547812cf436c60fc10c66ba3953ba9
Tusss <trinhtu500@gmail.com>
2023-08-12 09:46:54 +0700

    update code

--- TotalMEPProject/TotalMEPProject/UI/TotalMEPUI/VerticalMEPForm.Designer.cs
+++ TotalMEPProject/TotalMEPProject/UI/TotalMEPUI/VerticalMEPForm.Designer.cs
@@ -480,6 +480,7 @@
             this.ClientSize = new System.Drawing.Size(430, 253);
             this.Controls.Add(this.tableLayoutPanel9);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
